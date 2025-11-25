5c0b53cfa8464da7e009e269303128a99db68956
Evan Timmons <timmonsevan@gmail.com>
2025-11-25 12:43:56 -0500

    CRU is working in the front end

--- demo/src/main/java/com/mew/demo/controller/UserController.java
+++ demo/src/main/java/com/mew/demo/controller/UserController.java
@@ -10,7 +10,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,7 +18,8 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMethod;
+//import org.springframework.web.bind.annotation.RequestMethod;
+//import org.springframework.web.bind.annotation.CrossOrigin;
 
 @RequiredArgsConstructor
 @RestController
